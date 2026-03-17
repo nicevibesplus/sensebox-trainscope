e655dc9090b98edc037560d1179b26e9ab7fbefb
nicevibesplus <61326253+nicevibesplus@users.noreply.github.com>
2026-03-17 20:11:39 +0000

    style: automated code formatting

--- controller/controller.ino
+++ controller/controller.ino
@@ -20,119 +20,119 @@ BLERemoteCharacteristic* pRemoteCharacteristic = nullptr;
 BLEAddress* pServerAddress = nullptr;
 
 // Callback: Wird aufgerufen, wenn neue Daten vom Server gepusht werden
-static void notifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic,
-                           uint8_t* pData, size_t length, bool isNotify) {
-  currentGear = pData[0];
-  Serial.print("[BLE] Update empfangen! Neuer Gang: ");
-  Serial.println(currentGear);
+static void notifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length,
+                           bool isNotify) {
+    currentGear = pData[0];
+    Serial.print("[BLE] Update empfangen! Neuer Gang: ");
+    Serial.println(currentGear);
 }
 
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
-  void onResult(BLEAdvertisedDevice advertisedDevice) {
-    Serial.print("[Scan] Gerät gefunden: ");
-    Serial.println(advertisedDevice.getName().c_str());
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+        Serial.print("[Scan] Gerät gefunden: ");
+        Serial.println(advertisedDevice.getName().c_str());
 
-    if (advertisedDevice.getName() == bleServerName) {
-      Serial.println("[Scan] Treffer! TrainSense Server gefunden.");
-      advertisedDevice.getScan()->stop();
-      pServerAddress = new BLEAddress(advertisedDevice.getAddress());
-      deviceFound = true;
+        if (advertisedDevice.getName() == bleServerName) {
+            Serial.println("[Scan] Treffer! TrainSense Server gefunden.");
+            advertisedDevice.getScan()->stop();
+            pServerAddress = new BLEAddress(advertisedDevice.getAddress());
+            deviceFound = true;
+        }
     }
-  }
 };
 
 void setup() {
-  Serial.begin(115200);
-  delay(1000);  // Kurz warten, damit der Serial Monitor bereit ist
-  Serial.println("--- TrainSense Client Start ---");
+    Serial.begin(115200);
+    delay(1000);  // Kurz warten, damit der Serial Monitor bereit ist
+    Serial.println("--- TrainSense Client Start ---");
 
-  pinMode(EN_5V, OUTPUT);
-  digitalWrite(EN_5V, HIGH);
-  Serial.println("[Setup] 5V Ausgang aktiviert.");
+    pinMode(EN_5V, OUTPUT);
+    digitalWrite(EN_5V, HIGH);
+    Serial.println("[Setup] 5V Ausgang aktiviert.");
 
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
 
-  BLEDevice::init("TrainSense Client");
-  Serial.println("[BLE] Initialisierung abgeschlossen. Starte Scan...");
+    BLEDevice::init("TrainSense Client");
+    Serial.println("[BLE] Initialisierung abgeschlossen. Starte Scan...");
 
-  BLEScan* pBLEScan = BLEDevice::getScan();
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setActiveScan(true);
-  pBLEScan->start(30);
+    BLEScan* pBLEScan = BLEDevice::getScan();
+    pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+    pBLEScan->setActiveScan(true);
+    pBLEScan->start(30);
 }
 
 bool connectToServer() {
-  Serial.print("[Connect] Versuche Verbindung zu: ");
-  Serial.println(pServerAddress->toString().c_str());
+    Serial.print("[Connect] Versuche Verbindung zu: ");
+    Serial.println(pServerAddress->toString().c_str());
 
-  BLEClient* pClient = BLEDevice::createClient();
-  if (!pClient->connect(*pServerAddress)) {
-    Serial.println("[Fehler] Verbindung zum Server fehlgeschlagen.");
-    return false;
-  }
-  Serial.println("[Connect] Mit Server verbunden.");
+    BLEClient* pClient = BLEDevice::createClient();
+    if (!pClient->connect(*pServerAddress)) {
+        Serial.println("[Fehler] Verbindung zum Server fehlgeschlagen.");
+        return false;
+    }
+    Serial.println("[Connect] Mit Server verbunden.");
 
-  BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
-  if (pRemoteService == nullptr) {
-    Serial.println("[Fehler] Service UUID (181A) nicht gefunden.");
-    return false;
-  }
-  Serial.println("[Connect] Service gefunden.");
+    BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+    if (pRemoteService == nullptr) {
+        Serial.println("[Fehler] Service UUID (181A) nicht gefunden.");
+        return false;
+    }
+    Serial.println("[Connect] Service gefunden.");
 
-  pRemoteCharacteristic = pRemoteService->getCharacteristic(gearUUID);
-  if (pRemoteCharacteristic == nullptr) {
-    Serial.println("[Fehler] Charakteristik (2A56) nicht gefunden.");
-    return false;
-  }
-  Serial.println("[Connect] Charakteristik gefunden.");
+    pRemoteCharacteristic = pRemoteService->getCharacteristic(gearUUID);
+    if (pRemoteCharacteristic == nullptr) {
+        Serial.println("[Fehler] Charakteristik (2A56) nicht gefunden.");
+        return false;
+    }
+    Serial.println("[Connect] Charakteristik gefunden.");
 
-  if (pRemoteCharacteristic->canNotify()) {
-    pRemoteCharacteristic->registerForNotify(notifyCallback);
-    Serial.println("[Connect] Notifications erfolgreich abonniert.");
-  }
+    if (pRemoteCharacteristic->canNotify()) {
+        pRemoteCharacteristic->registerForNotify(notifyCallback);
+        Serial.println("[Connect] Notifications erfolgreich abonniert.");
+    }
 
-  return true;
+    return true;
 }
 
 int gearToAnalog(int gear) {
-  if (gear == 1) return 128;
-  if (gear == 2) return 192;
-  if (gear == 3) return 255;
-  return 0;
+    if (gear == 1) return 128;
+    if (gear == 2) return 192;
+    if (gear == 3) return 255;
+    return 0;
 }
 
 void loop() {
-  // Verbindungsaufbau falls Gerät gefunden wurde
-  if (deviceFound && pRemoteCharacteristic == nullptr) {
-    if (connectToServer()) {
-      Serial.println("[Status] System bereit. Warte auf Daten...");
-    } else {
-      Serial.println("[Status] Reconnect in 5 Sekunden...");
-      delay(5000);
+    // Verbindungsaufbau falls Gerät gefunden wurde
+    if (deviceFound && pRemoteCharacteristic == nullptr) {
+        if (connectToServer()) {
+            Serial.println("[Status] System bereit. Warte auf Daten...");
+        } else {
+            Serial.println("[Status] Reconnect in 5 Sekunden...");
+            delay(5000);
+        }
     }
-  }
 
-  // Motorsteuerung & Status-Meldung nur bei Änderung
-  int speed = gearToAnalog(currentGear);
+    // Motorsteuerung & Status-Meldung nur bei Änderung
+    int speed = gearToAnalog(currentGear);
 
-  if (currentGear != lastGear) {
-    Serial.print("[Motor] Modus: ");
-    Serial.print(forward ? "Vorwärts" : "Rückwärts");
-    Serial.print(" | Gang: ");
-    Serial.print(currentGear);
-    Serial.print(" | PWM: ");
-    Serial.println(speed);
-    lastGear = currentGear;  // Merken, damit wir nicht doppelt loggen
-  }
+    if (currentGear != lastGear) {
+        Serial.print("[Motor] Modus: ");
+        Serial.print(forward ? "Vorwärts" : "Rückwärts");
+        Serial.print(" | Gang: ");
+        Serial.print(currentGear);
+        Serial.print(" | PWM: ");
+        Serial.println(speed);
+        lastGear = currentGear;  // Merken, damit wir nicht doppelt loggen
+    }
 
-  if (forward) {
-    analogWrite(IN1, speed);
-    digitalWrite(IN2, LOW);
-  } else {
-    analogWrite(IN2, speed);
-    digitalWrite(IN1, LOW);
-  }
+    if (forward) {
+        analogWrite(IN1, speed);
+        digitalWrite(IN2, LOW);
+    } else {
+        analogWrite(IN2, speed);
+        digitalWrite(IN1, LOW);
+    }
 
-  delay(100);
+    delay(100);
 }
--- train/train.ino
+++ train/train.ino
@@ -19,84 +19,86 @@ String myMessage;
 int light_val = 0;
 
 class MyServerCallbacks : public BLEServerCallbacks {
-  void onConnect(BLEServer *pServer) { deviceConnected = true; };
+    void onConnect(BLEServer *pServer) {
+        deviceConnected = true;
+    };
 
-  void onDisconnect(BLEServer *pServer) { deviceConnected = false; }
+    void onDisconnect(BLEServer *pServer) {
+        deviceConnected = false;
+    }
 };
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Start Setup");
-  Wire.begin();
-  Wire.setClock(100000);  // ToF has max I2C freq of 1MHz
+    Serial.begin(115200);
+    Serial.println("Start Setup");
+    Wire.begin();
+    Wire.setClock(100000);  // ToF has max I2C freq of 1MHz
 
-  sensor_vl53l8cx.begin();
-  sensor_vl53l8cx.init();
-  sensor_vl53l8cx.set_resolution(VL53L8CX_RESOLUTION_8X8);
-  sensor_vl53l8cx.set_ranging_frequency_hz(30);
-  sensor_vl53l8cx.start_ranging();
-  Wire.setClock(100000);
+    sensor_vl53l8cx.begin();
+    sensor_vl53l8cx.init();
+    sensor_vl53l8cx.set_resolution(VL53L8CX_RESOLUTION_8X8);
+    sensor_vl53l8cx.set_ranging_frequency_hz(30);
+    sensor_vl53l8cx.start_ranging();
+    Wire.setClock(100000);
 
-  /*
-  if (!hdc.begin()) {
-    Serial.println("Couldn't find sensor!");
-    while (1);
-  }*/
+    /*
+    if (!hdc.begin()) {
+      Serial.println("Couldn't find sensor!");
+      while (1);
+    }*/
 
-  BLEDevice::init(bleServerName);
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+    BLEDevice::init(bleServerName);
+    BLEServer *pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
 
-  BLEService *pService = pServer->createService(
-      BLEUUID((uint16_t)0x181A));  // Environmental Sensing
-  pCharacteristic =
-      pService->createCharacteristic(BLEUUID((uint16_t)0x2A56),  // Sensor Value
-                                     BLECharacteristic::PROPERTY_NOTIFY);
+    BLEService *pService = pServer->createService(BLEUUID((uint16_t)0x181A));    // Environmental Sensing
+    pCharacteristic = pService->createCharacteristic(BLEUUID((uint16_t)0x2A56),  // Sensor Value
+                                                     BLECharacteristic::PROPERTY_NOTIFY);
 
-  pService->start();
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(pService->getUUID());
-  pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x0);
-  pAdvertising->setMinPreferred(0x1F);
-  BLEDevice::startAdvertising();
+    pService->start();
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(pService->getUUID());
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinPreferred(0x0);
+    pAdvertising->setMinPreferred(0x1F);
+    BLEDevice::startAdvertising();
 }
 
 void loop() {
-  if (deviceConnected) {
-    VL53L8CX_ResultsData Result;
-    uint8_t NewDataReady = 0;
-    uint8_t status;
+    if (deviceConnected) {
+        VL53L8CX_ResultsData Result;
+        uint8_t NewDataReady = 0;
+        uint8_t status;
 
-    Wire.setClock(1000000);  // Sensor has max I2C freq of 1MHz
-    status = sensor_vl53l8cx.check_data_ready(&NewDataReady);
-    Wire.setClock(100000);  // Display has max I2C freq of 100kHz
+        Wire.setClock(1000000);  // Sensor has max I2C freq of 1MHz
+        status = sensor_vl53l8cx.check_data_ready(&NewDataReady);
+        Wire.setClock(100000);  // Display has max I2C freq of 100kHz
 
-    if ((!status) && (NewDataReady != 0)) {
-      Wire.setClock(1000000);  // Sensor has max I2C freq of 1MHz
-      sensor_vl53l8cx.get_ranging_data(&Result);
-      Wire.setClock(100000);  // Display has max I2C freq of 100kHz
+        if ((!status) && (NewDataReady != 0)) {
+            Wire.setClock(1000000);  // Sensor has max I2C freq of 1MHz
+            sensor_vl53l8cx.get_ranging_data(&Result);
+            Wire.setClock(100000);  // Display has max I2C freq of 100kHz
 
-      int min_index = 0;
-      uint16_t min_distance = (long)(&Result)->distance_mm[0];
-      for (int i = 0; i < 64; i++) {
-        if ((long)(&Result)->distance_mm[i] <= min_distance) {
-          min_index = i;
-          min_distance = (long)(&Result)->distance_mm[i];
+            int min_index = 0;
+            uint16_t min_distance = (long)(&Result)->distance_mm[0];
+            for (int i = 0; i < 64; i++) {
+                if ((long)(&Result)->distance_mm[i] <= min_distance) {
+                    min_index = i;
+                    min_distance = (long)(&Result)->distance_mm[i];
+                }
+            }
+
+            Serial.println(min_distance);
+
+            if (min_distance <= 15) {
+                pCharacteristic->setValue(0);
+            } else {
+                pCharacteristic->setValue(1);
+            }
+
+            pCharacteristic->notify();
         }
-      }
 
-      Serial.println(min_distance);
-
-      if (min_distance <= 15) {
-        pCharacteristic->setValue(0);
-      } else {
-        pCharacteristic->setValue(1);
-      }
-
-      pCharacteristic->notify();
+        delay(100);
     }
-
-    delay(100);
-  }
 }
