879a2e9e18f62a5db98681329816e6daf7fd763d
janbecker2 <job02@gmx.de>
2026-03-18 18:01:52 +0100

    adding camera setup

--- src/train/train.ino
+++ src/train/train.ino
@@ -6,6 +6,8 @@
 #include <Wire.h>
 #include <vl53l8cx.h>
 #include <Adafruit_AS7341.h>
+#include "esp_camera.h"
+
 
 
 VL53L8CX sensor_vl53l8cx(&Wire, -1, -1);
@@ -57,11 +59,55 @@ void setup() {
     
     as7341.startReading();
 
-    /*
-    if (!hdc.begin()) {
-      Serial.println("Couldn't find sensor!");
-      while (1);
-    }*/
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sccb_sda = SIOD_GPIO_NUM;
+    config.pin_sccb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.frame_size = FRAMESIZE_QVGA;
+    config.pixel_format = PIXFORMAT_JPEG;
+    config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+    config.fb_location = CAMERA_FB_IN_PSRAM;
+    config.jpeg_quality = 4;
+    config.fb_count = 1;
+
+    if (config.pixel_format == PIXFORMAT_JPEG) {
+        if (psramFound()) {
+        config.jpeg_quality = 4;
+        config.fb_count = 1;
+        config.frame_size = FRAMESIZE_VGA;
+        config.grab_mode = CAMERA_GRAB_LATEST;
+        } else {
+        config.frame_size = FRAMESIZE_QVGA;
+        config.fb_location = CAMERA_FB_IN_DRAM;
+        }
+    } else {
+        config.frame_size = FRAMESIZE_240X240;
+        config.fb_count = 2;
+    }
+
+    Serial.println("init camera");
+    delay(3);
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed: 0x%x", err);
+        return;
+    }
 
     BLEDevice::init(bleServerName);
     BLEServer *pServer = BLEDevice::createServer();
@@ -108,11 +154,11 @@ void loop() {
 
             if (min_distance <= 50) {
                 pCharacteristic->setValue(0);
-                pCharacteristic->notify();
             } else {
                 pCharacteristic->setValue(2);
             }
 
+            /*
             while (!as7341.checkReadingProgress()) {
                 delay(10);
             }
@@ -123,42 +169,31 @@ void loop() {
 
             // Werte holen
             uint16_t red = as7341.getChannel(AS7341_CHANNEL_630nm_F7);   // Rot
-            uint16_t green = as7341.getChannel(AS7341_CHANNEL_555nm_F5); // Grün
+            uint16_t green = as7341.getChannel(AS7341_CHANNEL_515nm_F4); // Grün
             uint16_t blue = as7341.getChannel(AS7341_CHANNEL_480nm_F3);  // Blau
 
-            uint16_t maxVal = max(red, max(green, blue));
 
-            if (maxVal == 0) maxVal = 1;
 
-            uint8_t R = (uint32_t)red * 255 / maxVal;
-            uint8_t G = (uint32_t)green * 255 / maxVal;
-            uint8_t B = (uint32_t)blue * 255 / maxVal;
-
-            const uint16_t threshold = 100; 
-
-            if (maxVal < threshold) {
-                Serial.println("No Color Detected (Too Dark)");
-            } 
-            else if (R > G && R > B) {
-                // Red is the strongest channel
+            if (red > green && red > blue) {
                 Serial.println("Detected Red");
+            } 
+            else if (green > red && green > blue) {
+                // Red is the strongest channel
+                Serial.println("Detected green");
                 pCharacteristic->setValue(0); 
             } 
-            else if (G > R && G > B) {
+            else if (blue > red && blue > green) {
                 // Green is the strongest channel
-                Serial.println("Detected Green");
+                Serial.println("Detected blue");
                 // pCharacteristic->setValue(1); // Example for other colors
             } 
-            else if (B > R && B > G) {
-                // Blue is the strongest channel
-                Serial.println("Detected Blue");
-                // pCharacteristic->setValue(2);
-            }
             pCharacteristic->notify();
-        }
 
         as7341.startReading();
-
+        */
+        }
+        
+        pCharacteristic->notify();
         delay(100);
     }
 }
